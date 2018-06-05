b2ad5de5e30f92a925f063fd15612c34479579c7
Rob Ervin Jauquet <rob.jauquet@zapier.com>
2018-06-05 04:38:08 -0500

    adding ignore file and fixing up setup

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = 'django_search',
-    packages = ['django_search'],
-    version = '0.1',
-    description = 'Easy FTS with Django and PostgreSQL',
-    author = 'Rob Ervin Jauquet',
-    author_email = 'rjauquet@gmail.com',
-    url = 'https://github.com/rjauquet/django-search',
-    download_url = 'https://github.com/rjauquet/django-search/archive/0.1.tar.gz',
-    keywords = ['search', 'fts'],
-    classifiers = [],
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='django_search',
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    version='0.1',
+    description='Easy FTS with Django and PostgreSQL',
+    author='Rob Ervin Jauquet',
+    author_email='rjauquet@gmail.com',
+    url='https://github.com/rjauquet/django-search',
+    download_url='https://github.com/rjauquet/django-search/archive/0.1.tar.gz',
+    keywords=['search', 'fts'],
+    classifiers=[],
 )
