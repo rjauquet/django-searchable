e5ede2a79798aeb111c262b5d237ae4cc1696179
Rob Ervin Jauquet <rob.jauquet@zapier.com>
2018-06-05 02:56:51 -0500

    adding tests

--- tests/test_django_search.py
+++ tests/test_django_search.py
@@ -1,14 +1,28 @@
 from django.test import TestCase
+
 from django_search.models import SearchableModel, SearchableTextField
 from tests.models import TestModel
 
+
 class DjangoSearchModelTests(TestCase):
 
-    def test_init(self):
-
-        test_instance = TestModel.objects.create(
+    def setUp(self):
+        self.test_instance = TestModel.objects.create(
             text='searchable text goes here',
         )
-        test_instance.refresh_from_db()
-        self.assertTrue(test_instance.text)
-        self.assertTrue(test_instance.text_vector)
+        self.test_instance.refresh_from_db()
+
+    def test_init(self):
+        # text field should be set
+        self.assertTrue(self.test_instance.text)
+        # and the vector field should populate
+        self.assertEqual(self.test_instance.text_vector, "'goe':3 'searchabl':1 'text':2")
+
+    def test_vector_auto_update(self):
+        text = 'new stuff'
+        self.test_instance.text = text
+        # update and reload from db
+        self.test_instance.save()
+        self.test_instance.refresh_from_db()
+        # new vector shows up
+        self.assertEqual(self.test_instance.text_vector, "'new':1 'stuff':2")
