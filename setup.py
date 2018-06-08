5bb09de6e6d6ddca10bfb43c2e6d12c473a807f6
Rob Ervin Jauquet <rob.jauquet@zapier.com>
2018-06-08 10:37:02 -0500

    adding deps to setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,17 @@ with open("README.rst", "r") as fh:
 setuptools.setup(
     name='django_searchable',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'Django>=2.x',
+        'psycopg2-binary>=2.7.4',
+    ],
     long_description=long_description,
-    version='0.1',
+    version='0.1.1',
     description='Easy FTS with Django and PostgreSQL',
     author='Rob Ervin Jauquet',
     author_email='rjauquet@gmail.com',
     url='https://github.com/rjauquet/django-searchable',
-    download_url='https://github.com/rjauquet/django-searchable/archive/0.1.tar.gz',
+    download_url='https://github.com/rjauquet/django-searchable/archive/0.2.tar.gz',
     keywords=['search', 'searchable', 'fts'],
     classifiers=[],
 )
